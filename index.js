7bfbfa5fb425e5ee1e5b5d707b02477a63506234
writhyn <writhyn@gmail.com>
2020-04-11 18:56:39 -0400

    added all the things

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-let textArray = [];
-let blankArray = [];
+let proofText = '';
 
-const submitText = () => {
-    const text = document.querySelector('#practiceText').innerHTML;
-    textArray = text.split(' ');
-    blankArray = text.replace(/[a-z]/gi, '_').split(' ');
+const updateProofText = () => {
+    return proofText = document.querySelector('#practiceText').innerHTML;
 }
 
 const levelObj = {
@@ -23,7 +20,11 @@ const levelObj = {
     },
     lvlChange: function() {
         document.querySelector('#level').innerHTML = 'Level ' + this.level;
-        document.querySelector('h2').style.animationName = 'blink' + this.level;
+        document.querySelector('#practiceText').style.animationName = 'blink' + this.level;
+    },
+    lvlRefresh: function() {
+        this.level = 0;
+        this.lvlChange();
     }
 }
 
@@ -37,39 +38,86 @@ document.querySelector('#practiceText').addEventListener("paste", function(e) {
     // insert text manually
     document.execCommand("insertHTML", false, text);
 
-    submitText();
+    document.querySelector('#controls').classList.remove('invisible');
+
+    updateProofText();
 });
 
-document.querySelector('#instructLink').addEventListener('click', function() {
-    document.querySelector('#instructions').classList.toggle('hidden');
-});
 
 document.querySelector('#levelUp').addEventListener('click', () => levelObj.lvlUp());
 
 document.querySelector('#levelDown').addEventListener('click', () => levelObj.lvlDown());
 
+document.querySelector('#instructLink').addEventListener('click', function() {
+    if (document.querySelector('#review').classList.contains('unselected')) {
+        document.querySelector('#instructions'  ).classList.toggle('hidden');
+    } else {
+        document.querySelector('#instructions2').classList.toggle('hidden');
+    }
+});
 
 
+
+const unselectButton = () => {
+    document.querySelector('#memorize').classList.toggle('unselected');
+    document.querySelector('#review').classList.toggle('unselected');
+    document.querySelector('#instructions').classList.add('hidden');
+    document.querySelector('#instructions2').classList.add('hidden');
+}
 
 
 const reviewMode = () => {
+    unselectButton();
+    document.querySelector('#advance').classList.toggle('invisible');
+    updateProofText();
+    levelObj.lvlRefresh();
+    const text = document.querySelector('#practiceText');
+    text.contentEditable = 'false';
+    let textArray = proofText.split(' ');
+    let blankArray = proofText.replace(/[a-z]/gi, '_').split(' ');
+
     let index = 0;
-    document.querySelector('#practiceText').innerHTML = blankArray.join(' ');
-    window.addEventListener('keyup', (event) => {
-        const text = document.querySelector('#practiceText');
-        let result = event.key.toLowerCase();
-        if (result === textArray[index][0].toLowerCase()) {
-            blankArray[index] = textArray[index];
-            text.innerHTML = blankArray.join(' ');
-            index++;
+    
+    text.innerHTML = blankArray.join(' ');
+
+    const keyTest = () => {
+        if (document.querySelector('#memorize').classList.contains('unselected')) {
+            let result = event.key.toLowerCase();
+            if (result === textArray[index][0].toLowerCase()) {
+                blankArray[index] = textArray[index];
+                text.innerHTML = blankArray.join(' ');
+                index++;
+            }
+            if (textArray.slice(-1)[0] === blankArray.slice(-1)[0]) {
+                text.innerHTML = text.innerHTML + '<h2 style="text-align: center">Congrats!</h2>'
+                window.removeEventListener('keyup', keyTest);
+            }
         }
-    });
+    }
+
+    window.addEventListener('keyup', keyTest);
+
 }
 
-document.querySelector('#reviewMode').addEventListener('click', function() {
-    if (document.querySelector('#practiceText').innerHTML) {
-        document.querySelector('#advance').classList.toggle('invisible');
+const memorizeMode = () => {
+    unselectButton();
+    const text = document.querySelector('#practiceText');
+    text.contentEditable = 'true';
+    text.innerHTML = proofText;
+
+    document.querySelector('#advance').classList.toggle('invisible');
+    
+}
+
+document.querySelector('#review').addEventListener('click', function() {
+    if (proofText && this.classList.contains('unselected')) {
         reviewMode();
+    }
+});
+
+document.querySelector('#memorize').addEventListener('click', function() {
+    if (this.classList.contains('unselected')) {
+        memorizeMode();
     }
 });
 

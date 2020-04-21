a5a8f29c19db0bcb0e7c84b34725bfd830a4306d
Writhyn <writhyn@gmail.com>
2020-04-21 12:55:30 -0400

    final cleanup

--- index.js
+++ index.js
@@ -42,8 +42,6 @@ const memMode = {
         qS('#advance').classList.remove('invisible');
         qS('.mobile').classList.add('hidden');
 
-        qS('#doneMessage').classList.add('hidden');
-
         qS('#practiceText').contentEditable = 'true';
         qS('#practiceText').innerHTML = proofText.text;
         window.onkeyup = null;
@@ -67,11 +65,11 @@ const revMode = {
         "I can't think of any advice I need to give you. You have proven your competence.",
     ],
     fin: {
-        fail: 'Hmm. Maybe use "Memorize Mode" for a bit and come back for another try! You got this!',
-        close: 'Sooooooo close!<div id="tryAgain" class="myButtons">Give it another try!</div>I triple-dog dare you!',
-        tips: "(Click 'Instructions' for some extra tips!)",
-        practice: "(Don't forget to practice reciting out loud regularly!)",
-        success: function() {return revMode.congrats[Math.floor(Math.random() * revMode.congrats.length)]}
+        fail: '<h3 class="done">Hmm. Maybe use "Memorize Mode" for a bit and come back for another try! You got this!</h3>',
+        close: '<h3 class="done">Sooooooo close!<div id="tryAgain" class="myButtons">Give it another try!</div>I triple-dog dare you!</h3>',
+        tips: "<h4 class='doneSub'>(Click 'Instructions' for some extra tips!)</h4>",
+        practice: "<h4 class='doneSub'>(Don't forget to practice reciting out loud regularly!)</h4>",
+        success: function() {return '<h3 class="done">' + revMode.congrats[Math.floor(Math.random() * revMode.congrats.length)] + '</h3>'}
     },
     reviewMode: function() {
         qS('#memorize').classList.add('unselected');
@@ -93,7 +91,7 @@ const revMode = {
         let failNum = 0;
         
         
-        qS('#practiceText').innerHTML = blankArray.join(' ');
+        qS('#practiceText').innerText = blankArray.join(' ');
 
 
         const keyTest = (result) => {
@@ -116,13 +114,14 @@ const revMode = {
             
             if (blankArray.slice(-1)[0][0] !== '_') {
                 qS('.mobile').classList.add('hidden');
-                qS('#doneMessage').classList.remove('hidden');
                 if (failNum) {
-                    failNum >= textArray.length / 10 ? qS('#done').innerHTML = this.fin.fail : qS('#done').innerHTML = this.fin.close;
-                    qS('#doneSub').innerHTML = this.fin.tips;
+                    failNum >= textArray.length / 10 ?
+                        qS('#practiceText').insertAdjacentHTML('beforeend', this.fin.fail) :
+                        qS('#practiceText').insertAdjacentHTML('beforeend', this.fin.close);
+                    qS('#practiceText').insertAdjacentHTML('beforeend', this.fin.tips);
                 } else {
-                    qS('#done').innerHTML = this.fin.success();
-                    qS('#doneSub').innerHTML = this.fin.practice;
+                    qS('#practiceText').insertAdjacentHTML('beforeend', this.fin.success());
+                    qS('#practiceText').insertAdjacentHTML('beforeend', this.fin.practice);
                 }
             }
         }
@@ -142,6 +141,11 @@ const revMode = {
     },
 }
 
+const prepTextField = () => {
+    qS('#sample').classList.add('hidden');
+    qS('#shake').style.flexFlow = 'column nowrap';
+}
+
 qS('#practiceText').addEventListener("paste", e => {
     // cancel paste
     e.preventDefault();
@@ -149,25 +153,21 @@ qS('#practiceText').addEventListener("paste", e => {
     var text = (e.originalEvent || e).clipboardData.getData('text/plain');
     // insert text manually
     qS('#practiceText').innerHTML = text;
+    prepTextField();
 });
 
 qS('#practiceText').addEventListener('input', () => {
-    qS('#sample').classList.add('hidden');
-    qS('#shake').style.flexFlow = 'column nowrap';
+    prepTextField();
 });
 
 qS('#machine').addEventListener('click', event => {
     switch (event.target.id) {
         case 'sample':
-            qS('#sample').classList.add('hidden');
-            qS('#shake').style.flexFlow = 'column nowrap';
+            prepTextField();
             qS('#practiceText').innerText = 'This you know, my beloved brethren, but everyone must be quick to hear, slow to speak, and slow to anger; for the anger of man does not achieve the righteousness of God.';
-        case 'levelDown':
-            memMode.lvlDown();
             break;
-        case 'levelUp':
-            memMode.lvlUp();
-            break;
+        case 'levelDown': memMode.lvlDown(); break;
+        case 'levelUp': memMode.lvlUp(); break;
         case 'memorize':
             if (qS('#memorize').classList.contains('unselected')) {
                 memMode.memorizeMode();
@@ -188,5 +188,3 @@ qS('#machine').addEventListener('click', event => {
                 qS('#instructions2').classList.toggle('hidden');
     }
 })
-
-

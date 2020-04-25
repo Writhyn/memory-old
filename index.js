d78e122fa341758d9b2a49307643699d3d56e9ad
writhyn <writhyn@gmail.com>
2020-04-25 18:23:47 -0400

    added fluid typography to practiceText, including fallback

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const qS = document.querySelector.bind(document);
 
-console.log('Version: ', '1.0.1');
+console.log('Version: ', '1.0.2');
 
 const errorShake = (el) => {
     qS(el).classList.add('shake-horizontal');
